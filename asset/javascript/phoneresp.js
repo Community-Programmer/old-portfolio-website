dfa438e4fd4ad92c759209d509dc330a5460f72c
Sarthak Patel <sarthakpatel230204@gmail.com>
2024-01-19 21:51:32 +0530

    initial commit

---
+++ asset/javascript/phoneresp.js
@@ -0,0 +1,18 @@
+
+
+
+hamburger = document.querySelector('.hamburger')
+navbar = document.querySelector('.navbar')
+navlist = document.querySelector('.nav-list')
+rightnav = document.querySelector('.right-nav')
+
+
+
+
+hamburger.addEventListener('click',()=>{ 
+   rightnav.classList.toggle('v-class');
+   navlist.classList.toggle('v-class');
+   navbar.classList.toggle('h-nav');
+
+
+})
